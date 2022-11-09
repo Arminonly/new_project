00f446e4124fce637074989346d1ee9031e65eba
Arminonly <amiaby2016@gmail.com>
2022-11-09 22:04:35 +0300

    structure created

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-      <h1>test</h1>
+    <div className="app-wrapper">
+      <header className="header">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIe1y4-MXDb4XMXTOY6wQ50wosd_13iYqGFg&usqp=CAU"
+          alt=""
+        />
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">Main content</div>
     </div>
   );
 }
