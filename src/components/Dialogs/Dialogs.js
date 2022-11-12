0264f335b133ae8aa2946d517773b7daccdc8cfb
Arminonly <amiaby2016@gmail.com>
2022-11-12 18:15:52 +0300

    21-dialogs page grid created

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -2,5 +2,20 @@ import React from 'react';
 import s from './Dialogs.module.css';
 
 export default function Dialogs(props) {
-  return <div>Dialogs</div>;
+  return (
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog + ' ' + s.active}>Sergey</div>
+        <div className={s.dialog}>Alesya</div>
+        <div className={s.dialog}>Milana</div>
+        <div className={s.dialog}>Timosha</div>
+        <div className={s.dialog}>Arina</div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hello</div>
+        <div className={s.message}>How are you?</div>
+        <div className={s.message}>Who are you?</div>
+      </div>
+    </div>
+  );
 }
--- src/components/SettingsPage/SetPage.js
+++ src/components/SettingsPage/SetPage.js
@@ -2,5 +2,5 @@ import React from 'react';
 import s from './SetPage.module.css';
 
 export default function SetPage(props) {
-  return <div>SettinsPage</div>;
+  return <div>SettingsPage</div>;
 }
